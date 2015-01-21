4f16e80fce0c7c910c05a652f01e06a28764b1b6
Becky Smethurst <rebecca.smethurst@astro.ox.ac.uk>
2015-01-21 13:36:41 +0000

    Update starpy.py

--- starpy.py
+++ starpy.py
@@ -4,7 +4,7 @@ import numpy as N
 import sys
 
 # Use sys to assign arguments for the galaxy data from the command line
-u_r, err_u_r, nuv_u, err_nuv_u, z, pd, ps, dr8, ra, dec = sys.argv[1:]
+u_r, err_u_r, nuv_u, err_nuv_u, z, dr8, ra, dec = sys.argv[1:]
 
 # Use astropy to calculate the age from the redshift in the data 
 cosmo = FlatLambdaCDM(H0 = 71.0, Om0 = 0.26)
@@ -17,7 +17,7 @@ start = [7.5, 1.5] # starting place of all the chains
 burnin = 400 # number of steps in the burn in phase of the monte carlo chain
 
 #The rest calls the emcee module which is initialised in the sample function of the posterior file. 
-samples, samples_save = sample(2, nwalkers, nsteps, burnin, start, float(u_r), float(err_u_r), float(nuv_u), float(err_nuv_u), age, float(pd), float(ps), dr8)
+samples, samples_save = sample(2, nwalkers, nsteps, burnin, start, float(u_r), float(err_u_r), float(nuv_u), float(err_nuv_u), age, dr8)
 tq_mcmc, tau_mcmc,  = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*N.percentile(samples, [16,50,84],axis=0)))
 fig = corner_plot(samples, labels = [r'$ t_{quench}$', r'$ \tau$'], extents=[[N.min(samples[:,0]), N.max(samples[:,0])],[N.min(samples[:,1]),N.max(samples[:,1])]], bf=[tq_mcmc, tau_mcmc], id=dr8)
 fig.savefig('starpy_output_'+str(dr8)+'.pdf')
