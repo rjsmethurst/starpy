c8d5f0e58b707bcba17568757f4d13d266139e98
Becky Smethurst <rebecca.smethurst@astro.ox.ac.uk>
2015-01-21 12:48:47 +0000

    Update starpy.py

--- starpy.py
+++ starpy.py
@@ -17,8 +17,8 @@ start = [7.5, 1.5] # starting place of all the chains
 burnin = 400 # number of steps in the burn in phase of the monte carlo chain
 
 #The rest calls the emcee module which is initialised in the sample function of the posterior file. 
-s = sample(2, nwalkers, nsteps, burnin, start, float(u_r), float(err_u_r), float(nuv_u), float(err_nuv_u), age, float(pd), float(ps), dr8)
-tq_mcmc, tau_mcmc,  = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*N.percentile(s, [16,50,84],axis=0)))
-fig = corner_plot(samples, labels = [r'$ t_{quench}$', r'$ \tau$'], extents=[[N.min(s[:,0]), N.max(s[:,0])],[N.min(s[:,1]),N.max(s[:,1])]], bf=[tq_mcmc, tau_mcmc], id=dr8)
+samples, samples_save = sample(2, nwalkers, nsteps, burnin, start, float(u_r), float(err_u_r), float(nuv_u), float(err_nuv_u), age, float(pd), float(ps), dr8)
+tq_mcmc, tau_mcmc,  = map(lambda v: (v[1], v[2]-v[1], v[1]-v[0]), zip(*N.percentile(samples, [16,50,84],axis=0)))
+fig = corner_plot(samples, labels = [r'$ t_{quench}$', r'$ \tau$'], extents=[[N.min(samples[:,0]), N.max(samples[:,0])],[N.min(samples[:,1]),N.max(samples[:,1])]], bf=[tq_mcmc, tau_mcmc], id=dr8)
 fig.savefig('starpy_output_'+str(dr8)+'.pdf')
 print 'Best fit [t, tau] values found by starpy for input parameters are : [', tq_mcmc[0], tau_mcmc[0], ']'
